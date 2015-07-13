e281b91b7c368f47f5c2eede91ae39a24f1e538b
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2015-07-13 15:37:00 +0200

    Skeleton for the VME bridge process.

---
+++ BridgeVME.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "CAENVMElib.h"
+#include "CAENVMEoslib.h"
+#include "CAENVMEtypes.h"
+
+#include "VME_BridgeVx718.h"
+
+#include "CAEN_VME_TDC_addresses.h" // listing of addresses on TDC board (for instance kStatus)
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+if (argc!=2){
+     printf("One argument is required -- VME bridge device filename.\n");
+     return 1;
+    }
+else{
+    //uint16_t register_address = atoi(argv[1]);
+    char bridge_device_filename[100];
+    sscanf(argv[1], "%s", bridge_device_filename);
+    // sscanf(argv[2], "%x", &register_address);
+
+    printf("Got device filename %s\n", bridge_device_filename);
+
+    // printf("Got register address: 0x%04x (decimal -- %d)\nCAEN TDC Status register:0x%04x\n", register_address, register_address, kStatus);
+    // printf("Of board: 0x%08x\n", tdcboard_base_address);
+
+
+    VME::BridgeVx718 * bridge = new VME::BridgeVx718(bridge_device_filename, VME::CAEN_V2718);
+
+    int32_t bridge_handle = bridge->GetHandle();
+    printf("Initialized the VME Bridge at handle ID: %08x\n", bridge_handle);
+
+    char input[64];
+    while (1) { // read stdin, run CAEN call
+         printf ("Enter the VME Call (or help, quit): ");
+         scanf ("%63s", str);
+         if ( strcmp(str,"quit") != 0) printf("Sorry, only 'quit' is available now.\n");
+         else { delete bridge; return 0; }
+    }
+
+    // READING Register
+    // uint16_t value;
+    //CAENVME_ReadCycle(bridge_handle, tdcboard_base_address + kStatus, &value, cvA32_U_DATA, cvD16);
+    // CAENVME_ReadCycle(bridge_handle, tdcboard_base_address + register_address, &value, cvA32_U_DATA, cvD16);
+
+    // printf("Register at 0x%04x of board 0x%08x:\n", register_address, tdcboard_base_address);
+    // printf("value = 0x%04x\n", value);
+    delete bridge;
+    }
+
+}
+
