a56faac0e575b6f0d7a34b4b568f05cfc2e1e74c
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2018-04-02 00:53:20 +0100

    A bit of "protocolness" in comline input
    separated the parsing into a function
    -- usefull in making comline shortcuts
    
    	modified:   vmebus.py

--- daq/utils/vmebus.py
+++ daq/utils/vmebus.py
@@ -62,46 +62,52 @@ if err != cvSuccess.value:
 #s = create_string_buffer(b'0'*output_len)
 #err = lib.CAENVME_SWRelease(s)
 
-arguments = []
-output_arguments = []
-'''
-minilang to process/setup stuff
-'''
-for arg in args.arguments:
-    # most of commands require device handle
-    # only very few don't
-    # but for now for simplicity and generality
-    # always explicitly all arguments are passed from comline
-    # therefore parsing the device handle case:
+def parse_vme_arguments(comline_args):
+    '''parse_vme_arguments(comline_args)
 
-    if arg == 'dev':
-        arguments.append(dev)
-        continue
+    minilang parsing comline arguments into C function calls
+    '''
 
-    # char:20
-    if arg[:4] == 'char':
-        length = int(arg.split(',')[1])
-        the_argument = create_string_buffer(b'0'*length)
-    elif arg[:6] == 'uint32':
-        # type:value -- value is integer in hex!
-        address = int(arg.split(',')[1], 16)
-        the_argument = c_uint32(address)
-    elif arg[:2] == 'AM':
-        # AM:value (integer value)
-        value = int(arg.split(',')[1])
-        the_argument = CVAddressModifier_t(address)
-    elif arg[:2] == 'DW':
-        # DW:value (integer value)
-        value = int(arg.split(',')[1])
-        the_argument = CVDataWidth_t(address)
-    else:
-        logging.error('unknown argument arg: %s' % arg)
-        continue
+    all_arguments = []
+    output_arguments = []
+    for arg in comline_args:
+        # most of commands require device handle
+        # only very few don't
+        # but for now for simplicity and generality
+        # always explicitly all arguments are passed from comline
+        # therefore parsing the device handle case:
 
-    arguments.append(the_argument)
-    if 'out' in arg:
-        output_arguments.append(the_argument)
+        if arg == 'dev':
+            all_arguments.append(dev)
+            continue
 
+        # char:20
+        if arg[:4] == 'char':
+            length = int(arg.split(',')[1])
+            the_argument = create_string_buffer(b'0'*length)
+        elif arg[:6] == 'uint32':
+            # type:value -- value is integer in hex!
+            address = int(arg.split(',')[1], 16)
+            the_argument = c_uint32(address)
+        elif arg[:2] == 'AM':
+            # AM:value (integer value)
+            value = int(arg.split(',')[1])
+            the_argument = CVAddressModifier_t(address)
+        elif arg[:2] == 'DW':
+            # DW:value (integer value)
+            value = int(arg.split(',')[1])
+            the_argument = CVDataWidth_t(address)
+        else:
+            logging.error('unknown argument arg: %s' % arg)
+            continue
+
+        all_arguments.append(the_argument)
+        if 'out' in arg:
+            output_arguments.append(the_argument)
+
+    return all_arguments, output_arguments
+
+arguments, output_arguments = parse_vme_arguments(args.arguments)
 err = eval('lib.%s(*arguments)' % args.command)
 logging.debug('error: %s' % err)
 for a in output_arguments:
